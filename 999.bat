fdd4d21b9945d2eec36d8c3774c8dd62d3530e1c
ZeraX#1337 <43946069+furiosdestruct@users.noreply.github.com>
2020-09-13 06:37:45 -0700

    Add files via upload

---
+++ 999.bat
@@ -0,0 +1,11 @@
+@echo off
+
+color b
+
+cd C:\Windows\PLA
+
+Title cmd.exe
+
+kdmapper.exe driver.sys
+
+timeout 3
---
+++ Inject.bat
@@ -0,0 +1,9 @@
+@echo off
+
+cd C:\Windows\PLA
+
+Title C:\Windows\IME\Inject.bat
+
+modmap FortniteClient-Win64-Shipping.exe dxgi.dll ng6td6333.dll
+
+timeout 3
